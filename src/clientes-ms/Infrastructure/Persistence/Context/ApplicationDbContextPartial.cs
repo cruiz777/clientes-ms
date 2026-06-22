8c5d11502b105e914465f4e84d3cdb6017e6d94e
hermannsysgit <135393056+Andoresu23c@users.noreply.github.com>
2026-06-22 17:22:29 -0500

    feat: trigger auditoria con ef core para mapeo de entidad <> trigger

---
+++ src/clientes-ms/Infrastructure/Persistence/Context/ApplicationDbContextPartial.cs
@@ -0,0 +1,16 @@
+﻿using clientes_ms.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace clientes_ms.Infrastructure.Persistence.Context;
+
+public partial class ApplicationDbContext
+{
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Gln>(entity =>
+        {   //MAPEAR el esquema y nombre de la tabla
+            entity.ToTable("gln", "sic", t => t.HasTrigger("trg_gln_auditoria")); //Por norma de ef core es necesario mapear el trigger
+           
+        });
+    }
+}
